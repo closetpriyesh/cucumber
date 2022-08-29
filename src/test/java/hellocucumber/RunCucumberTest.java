69623047eda02558344044563e329fbef1f10382
Priyesh Kumar <priyesh_kumar@epam.com>
2022-08-29 15:34:28 +0530

    test

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("hellocucumber")
+@SelectClasspathResource("dynamicfeature")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
