9f3b3f6c7bf9f3684ea5e880dd177e7d85852872
Priyesh Kumar <priyesh_kumar@epam.com>
2022-08-30 12:36:34 +0530

    test

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/dynamicfeature",
-        plugin = {"pretty", "json:target/cucumber-json-reports/report.json"})
+        plugin = {"pretty", "json:target/cucumber-json-reports/report.json", "junit:target/cucumber-xml-reports/report.xml"})
 public class RunCucumberTest {
 }
